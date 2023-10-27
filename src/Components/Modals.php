ad967242ecdddbc3fa154404f029e2b1334ab185
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-10-27 16:01:47 +0330

    fix some issues.

--- src/Components/Modals.php
+++ src/Components/Modals.php
@@ -11,23 +11,24 @@ class Modals extends Component
     public $alias;
     public $params = [];
     public $activemodal;
-    public function render()
-    {
-        return view('livewire-bootstrap-modal::modals');
-    }
+
     #[On('showModal')]
     public function showModal($data)
     {
-        
         $this->alias = $data['alias'];
         $this->params = $data['params'];
         $this->activemodal = rand();
         $this->dispatch('showBootstrapModal');
     }
+
     #[On('resetModal')]
     public function resetModal()
     {
         $this->reset();
-       
+    }
+
+    public function render()
+    {
+        return view('livewire-bootstrap-modal::modals');
     }
 }
