4f652eec99471a3171e0b7d79a1f7134f12d5b2b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-13 10:44:42 +0330

    fix class.

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,4 +1,4 @@
-<div class="modal " id="livewire-bootstrap-modal" tabindex="-1" role="dialog" aria-hidden="true" wire:ignore.self
+<div class="modal" id="livewire-bootstrap-modal" tabindex="-1" role="dialog" aria-hidden="true" wire:ignore.self
     style="z-index: 9999;">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
