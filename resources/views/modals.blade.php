59cf31540b1027eeb9f3021370372ec9352c96df
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-01-13 10:48:42 +0330

    fix class.

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,7 +1,5 @@
-<div class="modal" id="livewire-bootstrap-modal" tabindex="-1" role="dialog" aria-hidden="true" wire:ignore.self
-    style="z-index: 9999;">
+<div class="modal" id="livewire-bootstrap-modal" tabindex="-1" aria-hidden="true" wire:ignore.self>
         @if ($alias)
             @livewire($alias, $params, key($activeModal))
         @endif
-    </div>
 </div>
